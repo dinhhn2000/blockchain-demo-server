740170814d7909b6edcde64e99aa64387e762292
dinhhn2000 <dinhhongoc@gmail.com>
2020-05-30 10:15:58 +0700

    fix cors issues

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 
 const app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
